173dac78986d8509769ff13275b7dc01c80f4e87
Wendell_paranhos <106284399+Wendell21212121212@users.noreply.github.com>
2022-08-06 00:29:52 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const nuvem = document.querySelector('.nuvem');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+
+    },500);
+}
+
+const loop = setInterval(() =>{
+     
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const nuvemPosition = nuvem.offsetLeft;
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        nuvem.style.animation = 'none';
+        nuvem.style.animation = `${nuvemPosition}px`;
+
+        mario.src = '/images/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+
+    }
+
+}, 10);
+
+
+document.addEventListener('keydown', jump);
